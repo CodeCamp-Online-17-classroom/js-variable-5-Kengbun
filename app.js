dcc3b439920fbf799286161d0515251d3a4c8713
keng <285keng+github@gmail.com>
2024-07-19 14:34:17 +0700

    ประกาศตัวแปร,ใช้คำสั่ง console.log and add name

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+let name = "Nantachai",
+    lastname = "Namwong",
+    age =25,
+    address = "99/70 บ้านกลาเมือง ";
+
+console.log("Full Name:",name,lastname,"Age :",age,"Address : ",address,".");
